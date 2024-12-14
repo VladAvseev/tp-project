fae6d3e497497a4b54e6dcfe8fab720afc56ee38
Nazar <n.shatilo@vibelab.ru>
2024-12-14 22:42:36 +0300

    Вынес в отдельную компоненту

---
+++ client/src/components/EmployeeLeaves.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+} from "@mui/material";
+import { ILeave } from "../interfaces/Leave.interface";
+import { addLeave, getLeavesByEmployee } from "../api/leave";
+
+interface EmployeeLeavesProps {
+  employeeId: string;
+  leaves: ILeave[];
+  setLeaves: React.Dispatch<React.SetStateAction<ILeave[]>>;
+}
+
+const EmployeeLeaves: React.FC<EmployeeLeavesProps> = ({
+  employeeId,
+  leaves,
+  setLeaves,
+}) => {
+  const [openDialog, setOpenDialog] = React.useState<boolean>(false);
+  const [newLeave, setNewLeave] = React.useState({
+    dateStart: "",
+    dateFinish: "",
+  });
+
+  const handleDialogOpen = () => setOpenDialog(true);
+  const handleDialogClose = () => setOpenDialog(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewLeave({ ...newLeave, [name]: value });
+  };
+
+  const handleAddLeave = async () => {
+    try {
+      const formattedLeave = {
+        dateStart: `${newLeave.dateStart}T00:00:00`,
+        dateFinish: `${newLeave.dateFinish}T00:00:00`,
+      };
+
+      await addLeave({ ...formattedLeave, employeeId });
+      const updatedLeaves = await getLeavesByEmployee(employeeId);
+      setLeaves(updatedLeaves);
+      handleDialogClose();
+    } catch (error) {
+      console.error("Error adding leave:", error);
+    }
+  };
+
+  const leavesColumns: GridColDef<ILeave>[] = [
+    { field: "id", headerName: "ID", flex: 0.2 },
+    {
+      field: "datestart",
+      headerName: "Начало",
+      flex: 0.4,
+      renderCell: ({ value }) => new Date(value).toLocaleDateString("ru-RU"),
+    },
+    {
+      field: "datefinish",
+      headerName: "Конец",
+      flex: 0.4,
+      renderCell: ({ value }) => new Date(value).toLocaleDateString("ru-RU"),
+    },
+  ];
+
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleDialogOpen}
+          sx={{ mb: 2 }}
+        >
+          Добавить
+        </Button>
+      </div>
+
+      <DataGrid rows={leaves} columns={leavesColumns} />
+      <Dialog
+        open={openDialog}
+        onClose={handleDialogClose}
+        sx={{ "& .MuiDialog-paper": { minWidth: 400 } }}
+      >
+        <DialogTitle>Добавить больничный</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Дата начала"
+            name="dateStart"
+            type="date"
+            value={newLeave.dateStart}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            label="Дата конца"
+            name="dateFinish"
+            type="date"
+            value={newLeave.dateFinish}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{ shrink: true }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="secondary">
+            Отменить
+          </Button>
+          <Button onClick={handleAddLeave} color="primary">
+            Сохранить
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default EmployeeLeaves;
